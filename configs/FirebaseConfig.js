c28051eb98048edf92ad9948116407cad8dfc819
Nguyễn Trọng Nghĩa <nguyentrongnghia2003@gmail.com>
2024-08-05 19:59:13 +0700

    add home screen header and config firebase

---
+++ configs/FirebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCO1b3MZLWvE2vXModk3skRv04V2yy8qiE",
+  authDomain: "react-native-businessdirectory.firebaseapp.com",
+  projectId: "react-native-businessdirectory",
+  storageBucket: "react-native-businessdirectory.appspot.com",
+  messagingSenderId: "847164692515",
+  appId: "1:847164692515:web:2681630cb4fbc2f0e07996",
+  measurementId: "G-KER1F6H3Q2"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
